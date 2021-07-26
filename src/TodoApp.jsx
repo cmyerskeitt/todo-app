68cd54185896e1410af46d78f5510a1cccb4a13b
github username <cmyerskeitt@gmail.com>
2021-07-26 13:56:23 -0400

    Create TodoContainer and TodoList

---
+++ src/TodoApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ToDoContainer from './components/TodoContainer';
+
+function TodoApp() {
+  return (
+    <div>
+      <ToDoContainer/>
+    </div>
+    
+  );
+}
+
+export default TodoApp;
---
+++ src/components/TodoContainer.js
@@ -0,0 +1,34 @@
+import React from "react"
+import TodoList from "./TodoList"
+
+class ToDoContainer extends React.Component {
+    state= {
+        todos: [
+            {
+                id:1,
+                title:"Setup development environment",
+                complete: true 
+            },
+            {
+                id: 2,
+                title:"Develop website and add content",
+                complete: false
+            },
+            {
+                id: 3,
+                title:"Deploy to live sever",
+                complete: false 
+            }
+        ]
+    }
+
+    render(){
+        return(
+            <div>
+                <TodoList todos={this.state.todos}/>
+            </div>
+        )
+    }
+
+}
+export default ToDoContainer
