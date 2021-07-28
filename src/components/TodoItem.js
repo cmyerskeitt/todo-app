aaa7358447b52049c1332cd791e7715c4fc9b57d
github username <cmyerskeitt@gmail.com>
2021-07-27 23:49:02 -0400

    create header and checkboxes for ToDoItems

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 class TodoItem extends React.Component {
     render(){
-        return <li> {this.props.todo.title} </li>
+        return <li> 
+            <input type="checkbox" checked={this.props.todo.complete} onChange={() =>console.log("clicked")}/>{this.props.todo.title} </li>
     }
 }
 export default TodoItem 
