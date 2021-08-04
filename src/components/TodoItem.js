7cf97f3a3eeecff8bf4cd5333a1386500d4225d8
github username <cmyerskeitt@gmail.com>
2021-08-03 23:50:01 -0400

    styled todo items

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,17 +1,30 @@
 import React from 'react' 
+import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
     render(){
-        return <li> 
-            <input type="checkbox" checked={this.props.todo.complete} onChange={() =>this.props.handleChangeProps(this.props.todo.id)}/>
-            {this.props.todo.title} <br>
+
+        const completedStyle = {
+            fontStyle: "italic",
+            color: "#595959",
+            opacity: 0.4,
+            textDecoration: "line-through",
+        }
+
+        return (
+        <li className={styles.item}> 
+            <input type="checkbox" className={styles.checkbox} checked={this.props.todo.complete} onChange={() =>this.props.handleChangeProps(this.props.todo.id)}/>
+            <span style={this.props.todo.complete ? completedStyle : null}>
+            {this.props.todo.title}
+            </span><br>
             </br>
             <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
                 Delete
             </button>
             
         </li>
-    }
+    )}
+
 }
 export default TodoItem 
 
